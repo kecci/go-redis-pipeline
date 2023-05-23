6549c9b3a9fb8b17e18a4b92bee3f0b5c6603f43
kecci <abyan.kecci@stockbit.com>
2023-05-23 10:47:04 +0700

    enhance with set & del

--- README.md
+++ README.md
@@ -1,26 +1,29 @@
 # go-redis-pipeline
 
 ## Usecase
-- Set 10.000 Keys.
-- Set 1 Keys.
+- Set & Del 1000 Keys.
+- Set & Del 1 Keys.
 
 ## Functions 
-1. singleCommandRedis - Set 10.000 keys using a single command per loop.
-2. pipelineRedis - Set 10.000 keys using a pipeline.
-3. singlePipelineRedis - Set 1 key using a pipeline.
+1. setCommand - Set keys with single command.
+2. setCommandPipeline - Set keys with pipeline.
+3. delCommand - Delete keys with single command.
+4. delCommandPipeline - Delete keys with pipeline.
 
 ## Output
 ```
-2023/05/19 23:53:30 processing 10000 keys time singleCommandRedis: 4.64399775s or 464.399µs/key
-2023/05/19 23:53:30 processing 10000 keys time pipelineRedis: 26.64325ms or 2.664µs/key
-2023/05/19 23:53:30 processing 1 keys time singlePipelineRedis: 472.917µs or 472.917µs/key
+# 1000 keys
+2023/05/23 10:42:03 processing 1000 keys time delCommandPipeline: 1.132291ms or 1.132µs/key
+2023/05/23 10:42:03 processing 1000 keys time delCommand: 887.208µs or 887ns/key
+2023/05/23 10:42:03 processing 1000 keys time setCommandPipeline: 4.451167ms or 4.451µs/key
+2023/05/23 10:42:03 processing 1000 keys time setCommand: 606.724667ms or 606.724µs/key
+
+# 1 key
+2023/05/23 10:45:31 processing 1 keys time delCommandPipeline: 569.542µs or 569.542µs/key
+2023/05/23 10:45:31 processing 1 keys time delCommand: 532µs or 532µs/key
+2023/05/23 10:45:31 processing 1 keys time setCommandPipeline: 574.833µs or 574.833µs/key
+2023/05/23 10:45:31 processing 1 keys time setCommand: 12.914542ms or 12.914542ms/key
 ```
-
-## Summary
-- Single 10.000 Keys -> 464.399µs/key
-- Pipeline 10.000 Keys -> 2.664µs/key
-- Pipeline 1 Key -> 472.917µs/key
-
 
 ## Conclusion
 We recommend using a pipeline to execute multiple commands, but we do not recommend using a pipeline if there is only one command because the result may be longer than a normal single command.
--- main.go
+++ main.go
@@ -9,34 +9,36 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-var (
-	totalKey = 10000
-)
-
 func main() {
+	totalKeys := 1
 	ctx := context.Background()
-
 	rdc := NewRedisClient()
 
-	nowSingle := time.Now()
-	rdc.singleCommandRedis(ctx)
-	durSingle := time.Since(nowSingle)
+	nowSet := time.Now()
+	rdc.setCommand(ctx, totalKeys)
+	durSet := time.Since(nowSet)
+	defer log.Printf("processing %v keys time setCommand: %+v or %+v/key\n", totalKeys, durSet, durSet/time.Duration(totalKeys))
 
-	nowPipe := time.Now()
-	rdc.pipelineRedis(ctx)
-	durPipe := time.Since(nowPipe)
+	nowSetPipe := time.Now()
+	rdc.setCommandPipeline(ctx, totalKeys)
+	durSetPipe := time.Since(nowSetPipe)
+	defer log.Printf("processing %v keys time setCommandPipeline: %+v or %+v/key\n", totalKeys, durSetPipe, durSetPipe/time.Duration(totalKeys))
 
-	nowSinglePipe := time.Now()
-	rdc.singlePipelineRedis(ctx)
-	durSinglePipe := time.Since(nowSinglePipe)
+	nowDel := time.Now()
+	rdc.delCommand(ctx, totalKeys)
+	durDel := time.Since(nowDel)
+	defer log.Printf("processing %v keys time delCommand: %+v or %+v/key\n", totalKeys, durDel, durDel/time.Duration(totalKeys))
 
-	log.Printf("processing %v keys time singleCommandRedis: %+v or %+v/key\n", totalKey, durSingle, time.Duration(durSingle.Nanoseconds()/int64(totalKey)))
-	log.Printf("processing %v keys time pipelineRedis: %+v or %+v/key\n", totalKey, durPipe, time.Duration(durPipe.Nanoseconds()/int64(totalKey)))
-	log.Printf("processing %v keys time singlePipelineRedis: %+v or %+v/key\n", 1, durSinglePipe, time.Duration(durSinglePipe.Nanoseconds()/1))
+	nowDelPipe := time.Now()
+	rdc.delCommandPipeline(ctx, totalKeys)
+	durDelPipe := time.Since(nowDelPipe)
+	defer log.Printf("processing %v keys time delCommandPipeline: %+v or %+v/key\n", totalKeys, durDelPipe, durDelPipe/time.Duration(totalKeys))
+
 }
 
 type RedisClient struct {
 	redisClient *redis.Client
+	expireAt    time.Time
 }
 
 func NewRedisClient() RedisClient {
@@ -49,55 +51,76 @@ func NewRedisClient() RedisClient {
 		MinIdleConns:    100,
 		ConnMaxIdleTime: time.Duration(300) * time.Second,
 	})
+
+	expireAt := time.Now().Add(time.Hour)
+
 	return RedisClient{
 		redisClient: cl,
+		expireAt:    expireAt,
 	}
 }
 
-func (r RedisClient) singleCommandRedis(ctx context.Context) {
-	for i := 1; i <= totalKey; i++ {
-		err := r.redisClient.HMSet(ctx, "redis-local:single", fmt.Sprintf("single_proccess_%d", i), i)
-		if err != nil {
-			log.Printf("%+v\n", err)
-			continue
+func (r RedisClient) setCommand(ctx context.Context, totalKeys int) {
+	for i := 1; i <= totalKeys; i++ {
+		args := map[string]string{
+			fmt.Sprintf("%d", i): fmt.Sprintf("%d", i),
 		}
+
+		key := fmt.Sprintf("redis-local:single:%d", i)
+		r.redisClient.HMSet(ctx, key, args)
+		r.redisClient.ExpireAt(ctx, key, r.expireAt)
 	}
 }
 
-func (r RedisClient) pipelineRedis(ctx context.Context) {
+func (r RedisClient) setCommandPipeline(ctx context.Context, totalKeys int) {
 	rdp := r.redisClient.Pipeline()
 
-	redisData := make(map[string]string)
-	for i := 1; i <= totalKey; i++ {
-		key := fmt.Sprintf("testing_key_redis_%d", i)
-		redisData[key] = fmt.Sprintf("%d", i)
+	for i := 1; i <= totalKeys; i++ {
+		args := map[string]string{
+			fmt.Sprintf("%d", i): fmt.Sprintf("%d", i),
+		}
+
+		key := fmt.Sprintf("redis-local:pipeline:%d", i)
+		rdp.HMSet(ctx, key, args)
+		rdp.ExpireAt(ctx, key, r.expireAt)
 	}
 
-	err := rdp.HMSet(ctx, "redis-local:pipeline", redisData).Err()
+	cmdr, err := rdp.Exec(ctx)
 	if err != nil {
-		log.Printf("%+v\n", err)
-	}
-
-	_, err = rdp.Exec(ctx)
-	if err != nil {
-		log.Printf("%+v\n", err)
+		errs := []error{}
+		for _, v := range cmdr {
+			errs = append(errs, v.Err())
+		}
+		panic(fmt.Errorf("%v - %v", err, errs))
 	}
 }
 
-func (r RedisClient) singlePipelineRedis(ctx context.Context) {
+func (r RedisClient) delCommand(ctx context.Context, totalKeys int) {
+	keys := []string{}
+	for i := 1; i <= totalKeys; i++ {
+		key := fmt.Sprintf("redis-local:single:%d", i)
+		keys = append(keys, key)
+	}
+	r.redisClient.Del(ctx, keys...)
+}
+
+func (r RedisClient) delCommandPipeline(ctx context.Context, totalKeys int) {
 	rdp := r.redisClient.Pipeline()
+	keys := []string{}
 
-	redisData := make(map[string]string)
-	key := fmt.Sprintf("testing_key_redis_%d", 1)
-	redisData[key] = fmt.Sprintf("%d", 1)
-
-	err := rdp.HMSet(ctx, "redis-local:single-pipeline", redisData).Err()
-	if err != nil {
-		log.Printf("%+v\n", err)
+	for i := 1; i <= totalKeys; i++ {
+		key := fmt.Sprintf("redis-local:pipeline:%d", i)
+		keys = append(keys, key)
 	}
 
-	_, err = rdp.Exec(ctx)
+	rdp.Del(ctx, keys...)
+
+	cmdr, err := rdp.Exec(ctx)
 	if err != nil {
-		log.Printf("%+v\n", err)
+		errs := []error{}
+		for _, v := range cmdr {
+			errs = append(errs, v.Err())
+		}
+		panic(fmt.Errorf("%v - %v", err, errs))
 	}
 }
